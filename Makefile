f6ef0f95c1a9e2bd983702e038a54b7c9fc2d20a
Brian Hays <hays1@purdue.edu>
2015-07-21 06:33:07 -0400

    added basic linked list functionality

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 all: visualize
 
 visualize: visualize.cpp visualize.h main.cpp
-	g++ -o visualize visualize.cpp main.cpp `pkg-config --cflags --libs gtkmm-3.0`
+	g++ -g -o visualize visualize.cpp main.cpp `pkg-config --cflags --libs gtkmm-3.0`
 
 clean:
 	rm *.o visualize
--- visualize.h
+++ visualize.h
@@ -3,6 +3,8 @@
 
 #include <gtkmm/drawingarea.h>
 
+
+
 class Visualize : public Gtk::DrawingArea {
 	public:
 		Visualize();
@@ -12,18 +14,52 @@ class Visualize : public Gtk::DrawingArea {
 		virtual bool on_draw(const Cairo::RefPtr<Cairo::Context> & cr);
 };
 
-class ListNode : public Gtk::Widget {
+class ListNode;
+
+class List : Gtk::Widget{
 	public:
-		ListNode(int x, int y);
+		List();
+		~List();
+		virtual void draw(const Cairo::RefPtr<Cairo::Context> & cr);
+		void newNode(ListNode * node);
+		void removeFromArray(ListNode * node);
+	private:
+		virtual void draw_arrow(const Cairo::RefPtr<Cairo::Context> & cr, int start_x, int start_y, int end_x, int end_y);
+		virtual void draw_null_arrow(const Cairo::RefPtr<Cairo::Context> & cr, int start_x, int start_y);
+
+	public:
+		ListNode * head;
+	private:
+		int list_x;
+		int list_y;
+		int out_x;
+		int out_y;
+		ListNode ** nodes;
+		int currentNodes;
+		static const int nodes_size;
+};
+
+class ListNode : public Gtk::Widget {
+	friend class List;
+	public:
+		ListNode();
+		ListNode(List * list);
+		ListNode(List * list, int x, int y);
 		~ListNode();
 		virtual void draw(const Cairo::RefPtr<Cairo::Context> & cr);
 		virtual void draw_text(const Cairo::RefPtr<Cairo::Context> & cr, int x, int y);
 		virtual void draw_node(const Cairo::RefPtr<Cairo::Context> & cr, int x, int y);
 
+	public:
+		ListNode * next;
 	private:
+		static const int field_w;
+		static const int field_h;
+		static const int padding;
+		List * list;
+		bool printed;
 		int x;
 		int y;
-		ListNode * next;
 };
 
 #endif
